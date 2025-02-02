3e42c939f0fd3d20e5e5a61cb5e5a27a3a3ccef1
Michael Rendall <michaelrendallmail@gmail.com>
2025-02-02 19:41:18 +0000

    26-code-structuring-for-bigger-projects

--- 3-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 3-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,3 +1,7 @@
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
+
 export default class Experience {
   constructor(canvas) {
     // Global access
@@ -5,5 +9,24 @@ export default class Experience {
 
     // Options
     this.canvas = canvas;
+
+    // Setup
+    this.sizes = new Sizes();
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+
+    // Sizes resize event
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // Time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
   }
+
+  resize() {}
+
+  update() {}
 }

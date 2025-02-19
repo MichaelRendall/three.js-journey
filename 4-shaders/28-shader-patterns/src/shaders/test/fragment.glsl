62049130edec311845230ab679f7712da22a3a02
Michael Rendall <michaelrendallmail@gmail.com>
2025-02-19 20:46:50 +0000

    28-shader-patterns

--- 4-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
+++ 4-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,16 @@
+#define PI 3.1415926535897932384626433832795
+
 varying vec2 vUv;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+    return vec2(
+        cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+        cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y);
+}
 
 void main() {
     // Pattern 1
@@ -77,9 +89,64 @@ void main() {
     // float strength = step(0.2, max(abs(vUv.x -0.5), abs(vUv.y -0.5)));
 
     // Pattern 20
-    float square1 = step(0.2, max(abs(vUv.x -0.5), abs(vUv.y -0.5)));
-    float square2 = 1.0 - step(0.25, max(abs(vUv.x -0.5), abs(vUv.y -0.5)));
-    float strength = square1 * square2;
+    // float square1 = step(0.2, max(abs(vUv.x -0.5), abs(vUv.y -0.5)));
+    // float square2 = 1.0 - step(0.25, max(abs(vUv.x -0.5), abs(vUv.y -0.5)));
+    // float strength = square1 * square2;
+
+    // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+
+    // Pattern 22
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+    // strength *= floor(vUv.y * 10.0) / 10.0;
+
+    // Pattern 23
+    // float strength = random(vUv);
+
+    // Pattern 24
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor(vUv.y * 10.0) / 10.0);
+    // float strength = random(gridUv);
+
+    // Pattern 25
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
+    // float strength = random(gridUv);
+
+    // Pattern 26
+    // float strength = length(vUv);
+
+    // Pattern 27
+    // float strength = length(vUv - 0.5);
+    // float strength = distance(vUv, vec2(0.5));
+
+    // Pattern 28
+    // float strength = 1.0 - distance(vUv, vec2(0.5));
+
+    // Pattern 29
+    // float strength = 0.015 / distance(vUv, vec2(0.5));
+
+    // Pattern 30
+    // vec2 lightUv = vec2(
+    //     vUv.x * 0.1 + 0.45,
+    //     vUv.y * 0.5 + 0.25);
+    // float strength = 0.015 / distance(lightUv, vec2(0.5));
+
+    // Pattern 31
+    // vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    // float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+
+    // vec2 lightUvY = vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25);
+    // float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    // float strength = lightX * lightY;
+
+    // Pattern 32
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+
+    vec2 lightUvX = vec2(rotatedUv.x * 0.1 + 0.45, rotatedUv.y * 0.5 + 0.25);
+    float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+
+    vec2 lightUvY = vec2(rotatedUv.y * 0.1 + 0.45, rotatedUv.x * 0.5 + 0.25);
+    float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    float strength = lightX * lightY;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }

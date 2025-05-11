cb9387636c74fb3be23bbc9bd9f2349efc089b35
Michael Rendall <michaelrendallmail@gmail.com>
2025-05-11 13:20:31 +0100

    34-fireworks-shaders

--- 4-shaders/34-fireworks-shaders/src/script.js
+++ 4-shaders/34-fireworks-shaders/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
 import gsap from "gsap";
+import { Sky } from "three/addons/objects/Sky.js";
 import fireworkVertexShader from "./shaders/firework/vertex.glsl";
 import fireworkFragmentShader from "./shaders/firework/fragment.glsl";
 
@@ -97,6 +98,7 @@ const createFirework = (count, position, size, texture, radius, color) => {
   // Geometry
   const positionsArray = new Float32Array(count * 3);
   const sizesArray = new Float32Array(count);
+  const timeMultipliersArray = new Float32Array(count);
 
   for (let i = 0; i < count; i++) {
     const i3 = i * 3;
@@ -113,7 +115,8 @@ const createFirework = (count, position, size, texture, radius, color) => {
     positionsArray[i3 + 1] = position.y;
     positionsArray[i3 + 2] = position.z;
 
-    sizesArray[i3] = Math.random();
+    sizesArray[i] = Math.random();
+    timeMultipliersArray[i] = 1 + Math.random();
   }
 
   const geometry = new THREE.BufferGeometry();
@@ -123,7 +126,11 @@ const createFirework = (count, position, size, texture, radius, color) => {
   );
   geometry.setAttribute(
     "aSize",
-    new THREE.Float32BufferAttribute(positionsArray, 1)
+    new THREE.Float32BufferAttribute(sizesArray, 1)
+  );
+  geometry.setAttribute(
+    "aTimeMultiplier",
+    new THREE.Float32BufferAttribute(timeMultipliersArray, 1)
   );
 
   // Material
@@ -163,25 +170,76 @@ const createFirework = (count, position, size, texture, radius, color) => {
     onComplete: destroy,
   });
 };
-createFirework(
-  100,
-  new THREE.Vector3(),
-  0.5,
-  textures[7],
-  1,
-  new THREE.Color("#8affff")
-);
 
-window.addEventListener("click", () => {
-  createFirework(
-    100,
-    new THREE.Vector3(),
-    0.5,
-    textures[7],
-    1,
-    new THREE.Color("#8affff")
+const createRandomFirework = () => {
+  const count = Math.round(400 + Math.random() * 1000);
+  const position = new THREE.Vector3(
+    (Math.random() - 0.5) * 2,
+    Math.random(),
+    (Math.random() - 0.5) * 2
   );
-});
+  const size = 0.1 + Math.random() * 0.1;
+  const texture = textures[Math.floor(Math.random() * textures.length)];
+  const radius = 0.5 + Math.random();
+  const color = new THREE.Color();
+  color.setHSL(Math.random(), 1, 0.7);
+
+  createFirework(count, position, size, texture, radius, color);
+};
+
+createRandomFirework();
+
+window.addEventListener("click", createRandomFirework);
+
+/**
+ * Sky
+ */
+// Add Sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+
+/// GUI
+
+const skyParameters = {
+  turbidity: 10,
+  rayleigh: 3,
+  mieCoefficient: 0.005,
+  mieDirectionalG: 0.95,
+  elevation: -2.2,
+  azimuth: 180,
+  exposure: renderer.toneMappingExposure,
+};
+
+const updateSky = () => {
+  const uniforms = sky.material.uniforms;
+  uniforms["turbidity"].value = skyParameters.turbidity;
+  uniforms["rayleigh"].value = skyParameters.rayleigh;
+  uniforms["mieCoefficient"].value = skyParameters.mieCoefficient;
+  uniforms["mieDirectionalG"].value = skyParameters.mieDirectionalG;
+
+  const phi = THREE.MathUtils.degToRad(90 - skyParameters.elevation);
+  const theta = THREE.MathUtils.degToRad(skyParameters.azimuth);
+
+  sun.setFromSphericalCoords(1, phi, theta);
+
+  uniforms["sunPosition"].value.copy(sun);
+
+  renderer.toneMappingExposure = skyParameters.exposure;
+  renderer.render(scene, camera);
+};
+
+gui.add(skyParameters, "turbidity", 0.0, 20.0, 0.1).onChange(updateSky);
+gui.add(skyParameters, "rayleigh", 0.0, 4, 0.001).onChange(updateSky);
+gui.add(skyParameters, "mieCoefficient", 0.0, 0.1, 0.001).onChange(updateSky);
+gui.add(skyParameters, "mieDirectionalG", 0.0, 1, 0.001).onChange(updateSky);
+gui.add(skyParameters, "elevation", 3, 10, 0.01).onChange(updateSky);
+gui.add(skyParameters, "azimuth", -180, 180, 0.1).onChange(updateSky);
+gui.add(skyParameters, "exposure", 0, 1, 0.0001).onChange(updateSky);
+
+updateSky();
 
 /**
  * Animate

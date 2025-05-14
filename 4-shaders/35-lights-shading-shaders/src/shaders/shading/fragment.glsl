d2f4a00a7142e5380215a2583fe67babf2e312fd
Michael Rendall <michaelrendallmail@gmail.com>
2025-05-14 20:49:47 +0100

    35-lights-shading-shaders

---
+++ 4-shaders/35-lights-shading-shaders/src/shaders/shading/fragment.glsl
@@ -0,0 +1,29 @@
+uniform vec3 uColor;
+
+varying vec3 vNormal;
+
+#include ../includes/ambientLight.glsl
+
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition) {
+    vec3 lightDirection = normalize(lightPosition);
+
+    // Shading
+    float shading = dot(normal, lightDirection);
+
+    return lightColor * lightIntensity * shading;
+}
+
+void main() {
+    vec3 color = uColor;
+
+    // Lights
+    vec3 light = vec3(0.0);
+    // light += ambientLight(vec3(1.0), 0.03);
+    light += directionalLight(vec3(0.1, 0.1, 1.0), 1.0, vNormal, vec3(0.0, 0.0, 3.0));
+    color *= light;
+
+    // Final color
+    gl_FragColor = vec4(color, 1.0);
+#include <tonemapping_fragment>
+#include <colorspace_fragment>
+}

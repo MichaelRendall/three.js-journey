1c6ee6c05d4ed7cc248823c81f5426e575f49993
Michael Rendall <michaelrendallmail@gmail.com>
2025-05-23 20:14:35 +0100

    39-particles-cursor-animation-shader

--- 4-shaders/39-particles-cursor-animation-shader/src/script.js
+++ 4-shaders/39-particles-cursor-animation-shader/src/script.js
@@ -105,7 +105,7 @@ displacement.glowImage.src = "./glow.png";
 
 displacement.interactivePlane = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshBasicMaterial({ color: "red" })
+  new THREE.MeshBasicMaterial({ color: "red", side: THREE.DoubleSide })
 );
 displacement.interactivePlane.visible = false;
 scene.add(displacement.interactivePlane);
@@ -116,6 +116,7 @@ displacement.raycaster = new THREE.Raycaster();
 // Coordinates
 displacement.screenCursor = new THREE.Vector2(9999, 9999);
 displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999);
 
 window.addEventListener("pointermove", (event) => {
   displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
@@ -129,6 +130,29 @@ displacement.texture = new THREE.CanvasTexture(displacement.canvas);
  * Particles
  */
 const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
+particlesGeometry.setIndex(null);
+particlesGeometry.deleteAttribute("normal");
+
+const intensitiesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+const anglesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+  intensitiesArray[i] = Math.random();
+  anglesArray[i] = Math.random() * Math.PI * 2;
+}
+
+particlesGeometry.setAttribute(
+  "aIntensity",
+  new THREE.BufferAttribute(intensitiesArray, 1)
+);
+particlesGeometry.setAttribute(
+  "aAngle",
+  new THREE.BufferAttribute(anglesArray, 1)
+);
 
 const particlesMaterial = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
@@ -181,11 +205,18 @@ const tick = () => {
     displacement.canvas.height
   );
 
+  // Speed alpha
+  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(
+    displacement.canvasCursor
+  );
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor);
+  const alpha = Math.min(cursorDistance * 0.1, 1);
+
   // Draw glow
   const glowSize = displacement.canvas.width * 0.25;
 
   displacement.context.globalCompositeOperation = "lighten";
-  displacement.context.globalAlpha = 1;
+  displacement.context.globalAlpha = alpha;
   displacement.context.drawImage(
     displacement.glowImage,
     displacement.canvasCursor.x - glowSize * 0.5,

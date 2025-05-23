e382450e9f26c336ae8aba913baad0c524727535
Michael Rendall <michaelrendallmail@gmail.com>
2025-05-23 17:20:16 +0100

    39-particles-cursor-animation-shader

--- 4-shaders/39-particles-cursor-animation-shader/src/script.js
+++ 4-shaders/39-particles-cursor-animation-shader/src/script.js
@@ -82,8 +82,8 @@ displacement.canvas = document.createElement("canvas");
 displacement.canvas.width = 128;
 displacement.canvas.height = 128;
 displacement.canvas.style.position = "fixed";
-displacement.canvas.style.width = "512px";
-displacement.canvas.style.height = "512px";
+displacement.canvas.style.width = "256px";
+displacement.canvas.style.height = "256px";
 displacement.canvas.style.top = 0;
 displacement.canvas.style.left = 0;
 displacement.canvas.style.zIndex = 10;
@@ -103,6 +103,28 @@ displacement.glowImage = new Image();
 displacement.glowImage.src = "./glow.png";
 // displacement.context.drawImage(displacement.glowImage, 20, 20, 32, 32);
 
+displacement.interactivePlane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+displacement.interactivePlane.visible = false;
+scene.add(displacement.interactivePlane);
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster();
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+
+window.addEventListener("pointermove", (event) => {
+  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
+  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
+// Texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas);
+
 /**
  * Particles
  */
@@ -119,6 +141,7 @@ const particlesMaterial = new THREE.ShaderMaterial({
       )
     ),
     uPictureTexture: new THREE.Uniform(textureLoader.load("./picture-1.png")),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
   },
 });
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
@@ -130,6 +153,49 @@ scene.add(particles);
 const tick = () => {
   // Update controls
   controls.update();
+
+  /**
+   * Raycaster
+   */
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  );
+
+  if (intersections.length) {
+    const uv = intersections[0].uv;
+
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width;
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height;
+  }
+
+  /**
+   * Displacement
+   */
+  displacement.context.globalCompositeOperation = "source-over";
+  displacement.context.globalAlpha = 0.02;
+  displacement.context.fillRect(
+    0,
+    0,
+    displacement.canvas.width,
+    displacement.canvas.height
+  );
+
+  // Draw glow
+  const glowSize = displacement.canvas.width * 0.25;
+
+  displacement.context.globalCompositeOperation = "lighten";
+  displacement.context.globalAlpha = 1;
+  displacement.context.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  );
+
+  // Texture
+  displacement.texture.needsUpdate = true;
 
   // Render
   renderer.render(scene, camera);

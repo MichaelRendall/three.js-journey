1519a69393a96928daf70cf7daad296295623a27
Michael Rendall <michaelrendallmail@gmail.com>
2025-08-17 20:40:34 +0100

    41-gpgpu-flow-field-particls-shaders

--- 4-shaders/41-gpgpu-flow-field-particles-shaders/src/script.js
+++ 4-shaders/41-gpgpu-flow-field-particles-shaders/src/script.js
@@ -142,7 +142,15 @@ gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [
 // Uniforms
 gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0);
 gpgpu.particlesVariable.material.uniforms.uDeltaTime = new THREE.Uniform(0);
-gpgpu.particlesVariable.material.uniforms.uBase = new THREE.Uniform(baseParticlesTexture);
+gpgpu.particlesVariable.material.uniforms.uBase = new THREE.Uniform(
+  baseParticlesTexture
+);
+gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence =
+  new THREE.Uniform(0.5);
+gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength =
+  new THREE.Uniform(2);
+gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency =
+  new THREE.Uniform(0.5);
 
 // Init
 gpgpu.computation.init();
@@ -155,6 +163,7 @@ gpgpu.debug = new THREE.Mesh(
       .texture,
   })
 );
+gpgpu.debug.visible = false;
 gpgpu.debug.position.x = 3;
 scene.add(gpgpu.debug);
 
@@ -231,6 +240,25 @@ gui
   .max(1)
   .step(0.001)
   .name("uSize");
+
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uFlowFieldInfluence");
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uFlowFieldStrength");
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uFlowFieldFrequency");
 
 /**
  * Animate
